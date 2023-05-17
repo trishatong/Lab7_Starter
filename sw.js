ff8679f779b6fad149c0d7629407f004dce90048
trishatong <trtong1@gmail.com>
2023-05-17 11:52:41 -0700

    replaced fetch(event.request.url) with fetch(event.request)

--- sw.js
+++ sw.js
@@ -45,7 +45,7 @@ self.addEventListener('fetch', function (event) {
       // B8. TODO - If the request is in the cache, return with the cached version.
       //            Otherwise fetch the resource, add it to the cache, and return
       //            network response.
-      return cache.match(event.request.url).then((cachedResponse) => {
+      return cache.match(event.request).then((cachedResponse) => {
         if (cachedResponse) {
           return cachedResponse;
         }
